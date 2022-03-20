a4db6f57e5803aa8d94fc8c7ecbba062226ff5f8
Gamer43 <towerdefence1000@gmail.com>
2022-03-20 16:48:49 -0700

    don't crash if connection refused

--- TCPClient.py
+++ TCPClient.py
@@ -11,17 +11,17 @@ class TCPClient(threading.Thread):
     #receive callback should take two arguments, message (str) and address (tuple)
     def __init__(self, server_ip, port, receive_callback = None, timeout = 5):
         super(TCPClient, self).__init__()
-        
+
         self.sel = selectors.DefaultSelector()
         self.receive_callback = receive_callback
         #initialize the socket.
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((server_ip, port))
+        self.socket.connect_ex((server_ip, port))
         events = selectors.EVENT_READ | selectors.EVENT_WRITE
         data = types.SimpleNamespace(transmitBuffer=b"", receiveBuffer=b"")
-        
+
         self.sel.register(self.socket, events, data=data)
-        
+
         self.active = True
         self.sendBuffer = deque()
 
@@ -47,7 +47,7 @@ class TCPClient(threading.Thread):
                 data.transmitBuffer = ""
     def send(self, message):
         self.sendBuffer.append(bytes(message, encoding="UTF-8"))
-            
+
      #must be overridden when inheriting form threading.Thread
     def run(self):
         print("Starting TCP Socket")
@@ -55,7 +55,10 @@ class TCPClient(threading.Thread):
         while(self.active):
             events = self.sel.select(timeout=0)
             for key, mask in events:
-                self.service_connection(key, mask)
+                try:
+                    self.service_connection(key, mask)
+                except:
+                    pass
             time.sleep(0.001)
         self.socket.close()
         self.sel.close()
